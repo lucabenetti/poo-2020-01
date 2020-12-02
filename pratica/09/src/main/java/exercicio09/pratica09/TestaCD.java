df1be2ff4215e43a4416c6777242c75882c40b9b
lucabenetti <lucabenetti@discente.ufg.br>
2020-12-02 16:05:58 -0300

    Adiciona exercicio 09

---
+++ pratica/09/src/main/java/exercicio09/pratica09/TestaCD.java
@@ -0,0 +1,16 @@
+package exercicio09.pratica09;
+
+import java.util.Date;
+
+public class TestaCD {
+
+    public static void main(String[] args) {
+        Pessoa zeze = new Pessoa("Zezé di Camargo");
+        Pessoa luciano = new Pessoa("Luciano");
+
+        CD zezeLuciano = new CD("Roça", new Date(), zeze);
+        zezeLuciano.adicionaCantor(luciano);
+
+        System.out.println(zezeLuciano);
+    }
+}
